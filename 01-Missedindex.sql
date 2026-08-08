ce2ea0aa4c022570182a33c596177dee838925d8
Elham Asadi <eliasadi362@gmail.com>
2026-08-08 10:44:42 +0330

    Update missing index query with user updates

--- 01-Missedindex.sql
+++ 01-Missedindex.sql
@@ -1,26 +1,36 @@
 SELECT
-
     PARSENAME(REPLACE(REPLACE(mid.statement, '[', ''), ']', ''), 3) AS DatabaseName,
     PARSENAME(REPLACE(REPLACE(mid.statement, '[', ''), ']', ''), 2) AS SchemaName,
-    PARSENAME(REPLACE(REPLACE(mid.statement, '[', ''), ']', ''), 1) AS TableName ,
+    PARSENAME(REPLACE(REPLACE(mid.statement, '[', ''), ']', ''), 1) AS TableName,
     migs.avg_user_impact,
-	migs.user_scans,
-	migs.user_seeks,
-    CONVERT (varchar(30), getdate(), 126) AS runtime,
-   'CREATE INDEX index_'+PARSENAME(REPLACE(REPLACE(mid.statement, '[', ''), ']', ''), 1)+ '  ON  ' + mid.statement + ' (' + ISNULL (mid.equality_columns, '') + CASE
-    WHEN mid.equality_columns IS NOT NULL
-    AND mid.inequality_columns IS NOT NULL THEN ','
-    ELSE ''
-  END + ISNULL (mid.inequality_columns, '') + ')' + ISNULL (' INCLUDE (' + mid.included_columns + ')', '') AS create_index_statement
-  --,
-  --migs.* 
-  
+    migs.user_scans,
+    migs.user_seeks,
+    ISNULL(ius.user_updates, 0) AS user_updates,
+    CONVERT(varchar(30), GETDATE(), 126) AS runtime,
+    'CREATE INDEX index_' + PARSENAME(REPLACE(REPLACE(mid.statement, '[', ''), ']', ''), 1) +
+    ' ON ' + mid.statement + ' (' +
+    ISNULL(mid.equality_columns, '') +
+    CASE
+        WHEN mid.equality_columns IS NOT NULL
+         AND mid.inequality_columns IS NOT NULL THEN ','
+        ELSE ''
+    END +
+    ISNULL(mid.inequality_columns, '') + ')' +
+    ISNULL(' INCLUDE (' + mid.included_columns + ')', '') AS create_index_statement
 FROM sys.dm_db_missing_index_groups mig
-	INNER JOIN sys.dm_db_missing_index_group_stats migs ON migs.group_handle = mig.index_group_handle
-	INNER JOIN sys.dm_db_missing_index_details mid ON mig.index_handle = mid.index_handle
-WHERE migs.avg_user_impact>30
-ORDER BY 
-migs.avg_user_impact,
-user_seeks desc
----,
---user_scans 
+INNER JOIN sys.dm_db_missing_index_group_stats migs
+    ON migs.group_handle = mig.index_group_handle
+INNER JOIN sys.dm_db_missing_index_details mid
+    ON mig.index_handle = mid.index_handle
+OUTER APPLY (
+    SELECT TOP (1) us.user_updates
+    FROM sys.dm_db_index_usage_stats us
+    WHERE us.database_id = mid.database_id
+      AND us.object_id = mid.object_id
+      AND us.index_id IN (0, 1)   -- heap / clustered
+    ORDER BY CASE WHEN us.index_id = 1 THEN 0 ELSE 1 END
+) ius
+WHERE migs.avg_user_impact > 30
+ORDER BY
+    migs.avg_user_impact,
+    migs.user_seeks DESC;
